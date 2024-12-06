d4c047359cdbd129951b5a877b4e78b3ab58f385
ETL-H <itiel.alevi.g@gmail.com>
2024-12-06 23:46:02 +0200

    Revert "Num1 working for all cases. still need to add the num2 and to print what the value is when there is an error. plus need to get rid of alot of useless things"
    This reverts commit b2202a0baf13f1bbc1cdad83a65cf669c3b2eb36.

--- Ex1Main.java
+++ Ex1Main.java
@@ -16,22 +16,18 @@ public class Ex1Main {
         String num1 = "", num2="", quit = "quit", base ="";
         while (!num1.equals(quit) && !num2.equals(quit)) {
             System.out.println();
+            System.out.println("Ex1 class solution:");
             System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
             num1 = sc.next();
-            boolean flag = true;
             boolean ans1 = Ex1.isNumber(num1);
             if(!num1.equals("quit")) {
                 System.out.printf(num1 + " is number: " + ans1 + " value = " + Ex1.number2Int(num1));
                 System.out.println();
-                if (isNumber(num1)){
+                System.out.println("Ex1 class solution:");
                 System.out.println("Enter a string as number#2 (or \"quit\" to end the program): ");
                 num2 = sc.next();
-                }
-                else{
-                    flag = false;
-                }
             }
-            if(!num2.equals(quit) && flag){
+            if(!num2.equals(quit) && ans1){
                 boolean ans2 = Ex1.isNumber(num2);
                 System.out.printf(num2 + " is number: " + ans2 + " value = " + Ex1.number2Int(num2));
             }
